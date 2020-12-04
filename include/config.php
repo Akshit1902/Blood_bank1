677159865a55471b080ab0c43a8026f70c33a3ac
Akshit1902 <32017846+Akshit1902@users.noreply.github.com>
2020-12-04 15:17:25 +0530

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,22 +1,39 @@
 <?php
-    session_start();
-    
-    // db configration
-    $server="localhost";
-    $username="root";
-    $password="";
-    $database="blood_bank";
-       
-    date_default_timezone_set('Asia/Kolkata');
-    $con=mysqli_connect($server,$username,$password,$database) or die ("could not connect to mysql");
+/*
+ * --------------------------------------------------------------------
+ * LOAD THE CUSTOM FILE CONFIGURATIONS
+ * --------------------------------------------------------------------
+ *
+ */
+// configure enviroment / set true if it is in live server
+$PRODUCTION = true;
+define('PRODUCTION', $PRODUCTION);
 
-    // some global value
-    $DATETIME_FORMAT="l, M j, Y @ g:ia";
-    $HOSTNAME="http://localhost/blood_bank";
-    $COMPANY_NAME="Blood Bank";
-    $COMPANY_ADDRESS="Visakhapatnam";
-    $COMPANY_MOBILE="+91 9491207148";
-    $COMPANY_EMAIL="srujan.aakurathi@yahoo.co.in";
+$BASE_URL = (PRODUCTION) ? "https://internshala-project1.herokuapp.com" : "http://localhost/nazish/blood_bank";
 
-    require_once 'function.php';
+define('BASE_URL', $BASE_URL);
+
+
+
+/**
+ * Configuration for database
+ */
+$HOSTNAME = (PRODUCTION) ? "sql12.freemysqlhosting.net" : "localhost";
+$USERNAME = (PRODUCTION) ? "sql12380200" : "root";
+$PASSWORD = (PRODUCTION) ? "ssmJ2XfQbw" : "root";
+$DATABASE = (PRODUCTION) ? "sql12380200" : "blood_bank";
+$PREFIX   = (PRODUCTION) ? "bb_" : "bb_";
+
+define('HOSTNAME', $HOSTNAME);
+define('USERNAME', $USERNAME);
+define('PASSWORD', $PASSWORD);
+define('DATABASE', $DATABASE);
+define('PREFIX'  , $PREFIX);
+
+define('MAINTENANCE_MODE', false);
+define('ENCRYPTION_KEY', '');
+
+define('EMAIL_API', '');
+
+
 ?>
